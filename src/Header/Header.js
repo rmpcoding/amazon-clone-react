0cfdd92b096d933f671553ac82f4abb6413273cd
Robert Parsons <rmpcoding@gmail.com>
2020-12-15 19:05:14 -0800

    Adds Header component with defined height and color, as well as navbar component

---
+++ src/Header/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+// import NavBar from '../NavBar/NavBar'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img
+                className="header__logo"
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="Amazon logo"
+            />
+            <div className="header__search">
+                <input className="header__searchInpug" type="text"></input>
+            </div>
+
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Guest</span>
+                    <span className="header__optionLineTwo">Sign In</span>
+                </div>
+
+                <div className="header__option">
+                    {' '}
+                    <span className="header__optionLineOne">Returns</span>
+                    <span className="header__optionLineTwo">& Orders</span>
+                </div>
+                <div className="header__option">
+                    {' '}
+                    <span className="header__optionLineOne">Your</span>
+                    <span className="header__optionLineTwo">Prime</span>
+                </div>
+                {/* <NavBar/> */}
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB-0d7fEgeeaX5TAf6s2Nu5wgzeDbHbaxY",
+    authDomain: "clone-f7b9a.firebaseapp.com",
+    projectId: "clone-f7b9a",
+    storageBucket: "clone-f7b9a.appspot.com",
+    messagingSenderId: "902655806469",
+    appId: "1:902655806469:web:6572a1eedf8f09d4b3606c",
+    measurementId: "G-6KND6YW6EC"
+  };
+
+
+
+// export default { auth, db, storage } 
