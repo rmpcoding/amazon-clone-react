a05890f18bc7b59ff7744d0b03bd69033f2e7b97
Robert Parsons <rmpcoding@gmail.com>
2020-12-17 20:14:16 -0800

    Adds react-router-dom dependency, and some route functionality to App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import Header from './Header/Header';
 import Home from './Home/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
     return (
-        <div className="app">
-            <Header />
-            <Home />
-        </div>
+        <Router>
+            <div className="app">
+                <Switch>
+                    <Route path="/">
+                        <Header />
+                        <Home />
+                    </Route>
+                    <Route path="/checkout">
+                        <Header />
+                        I AM CHECKOUT PAGE
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
